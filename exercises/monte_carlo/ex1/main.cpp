215722702e656ed1998cc445d8fdffc99f447558
physicshinzui@gmail.com <physicshinzui@gmail.com>
2021-04-21 18:12:18 +0900

    trivial

--- exercises/monte_carlo/ex1/main.cpp
+++ exercises/monte_carlo/ex1/main.cpp
@@ -11,25 +11,22 @@ double E_harmonic(double x, double x_eq, double k_spring = 1.0)
     return 0.5 * k_spring * (x - x_eq) * (x - x_eq);
 }
 
-void metropolisHasting(int n_steps)
+void metropolisHasting(int n_steps, double step_size, double KT, int seed, std::string foutname = "traj.dat")
 {
-    //parameters 
-    double KT {1.0};
-    double c {1.0};
-
     //Initialisation
     double xi {0.1};
-    double x_eq {1.0};
+    double x_eq {0.0};
     double dx{};
     int    n_accept {0};
 
     std::ofstream trajfile;
-    trajfile.open("traj.dat");
+    trajfile.open(foutname);
 
-    std::srand(10);
-    for (int i{0}; i < n_steps; ++i) {
+    std::srand(seed);
+    for (int i{0}; i < n_steps; ++i) 
+    {
         dx = static_cast<double>(std::rand()) / RAND_MAX - 0.5; //ranging [-0.5, 0.5]
-        dx = c * dx;
+        dx = 2.0 * step_size * dx;
         double x_suc = xi + dx;
         double E_pre = E_harmonic(xi   , x_eq);        
         double E_suc = E_harmonic(x_suc, x_eq);
@@ -43,7 +40,10 @@ void metropolisHasting(int n_steps)
             xi = x_suc;
             ++n_accept;
         }
-        else { }
+        else 
+        { 
+            continue; //Reject
+        }
     }
     std::cout << "Accept ratio = " << (double)n_accept / n_steps << std::endl;
     trajfile.close();
@@ -51,7 +51,10 @@ void metropolisHasting(int n_steps)
 
 int main()
 {
-    int n_steps {10000};
-    metropolisHasting(n_steps);
+    int n_steps {1000000};
+    double step_size {1.0};
+    int seed{10};
+    double KT{0.1};
+    metropolisHasting(n_steps, step_size, KT, seed);
     return 0;
 }
