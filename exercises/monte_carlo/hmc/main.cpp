a96004494b6be7cc8518641a372f95ffe269fe03
physicshinzui@gmail.com <physicshinzui@gmail.com>
2021-04-22 08:53:09 +0900

    variables were renamed

--- exercises/monte_carlo/hmc/main.cpp
+++ exercises/monte_carlo/hmc/main.cpp
@@ -12,9 +12,9 @@ double E_harmonic(double x, double x_eq, double k_spring = 1.0)
     return 0.5 * k_spring * (x - x_eq) * (x - x_eq);
 }
 
-double kinetic_energy(double pi)
+double kinetic_energy(double p_tau)
 {
-    return 0.5 * pi * pi;
+    return 0.5 * p_tau * p_tau;
 }
 
 void HMC(int n_steps, double d_tau)
@@ -23,12 +23,12 @@ void HMC(int n_steps, double d_tau)
     std::normal_distribution<double>  normal_dist(0.0, 1.0);
 
     // Initialisation at tau = 0
-    double xi {1.0};
+    double x_tau {1.0};
     double x_eq {0.0};
-    double pi {normal_dist(generator)};
-    double ai {spring_force(xi, x_eq)}; 
-    double PE {E_harmonic(xi, x_eq)};
-    double KE {kinetic_energy(pi)};
+    double p_tau {normal_dist(generator)};
+    double a_tau {spring_force(x_tau, x_eq)}; 
+    double PE {E_harmonic(x_tau, x_eq)};
+    double KE {kinetic_energy(p_tau)};
     double H_init {PE + KE}; 
     
     std::ofstream trajfile; 
@@ -37,18 +37,18 @@ void HMC(int n_steps, double d_tau)
     std::srand(10);
     for (int i {0}; i < n_steps; ++i)
     {
-        double pi_half = pi + 0.5 * ai * d_tau;
-        double x_suc = xi + pi_half * d_tau;
+        double p_tau_half = p_tau + 0.5 * a_tau * d_tau;
+        double x_suc = x_tau + p_tau_half * d_tau;
         double a_suc = spring_force(x_suc, x_eq);
-        double p_suc = pi_half + 0.5 * a_suc * d_tau;
+        double p_suc = p_tau_half + 0.5 * a_suc * d_tau;
         double H_fin = E_harmonic(x_suc, x_eq) + kinetic_energy(p_suc);
         double r = static_cast<double>(std::rand()) / RAND_MAX;
         if (r < exp(H_init - H_fin) ) 
         {
-            xi = x_suc;
-            pi = p_suc;
+            x_tau = x_suc;
+            p_tau = p_suc;
         }
-        trajfile << xi << ' ' << pi << '\n';
+        trajfile << x_tau << ' ' << p_tau << '\n';
     }
     trajfile.close();
 }
